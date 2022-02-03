b785fe9788ea2797ad0fed731c8c15b99e380016
Dmitry Utkin <senigius8@gmail.com>
2022-02-03 14:55:44 +0300

    added eslint, connected guthub actions

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,6 +1,9 @@
 import readLineSync from 'readline-sync';
-export const askName = (name) => {
-    name = readLineSync.question('Please, tell me your name: ');
-    if (name === '') return 'anon';
-    return name;
+
+const askName = (name) => {
+  let yourName = name;
+  yourName = readLineSync.question('Please, tell me your name: ');
+  if (yourName === '') return 'anon';
+  return yourName;
 };
+export default askName;
