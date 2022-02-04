c63815aab707befa4108a8a58fd45594b9ddb629
Dmitry Utkin <senigius8@gmail.com>
2022-02-04 18:32:00 +0300

    исправлена игра на четность, изменен параметр по которому выбирается случайное число\

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 import askName from '../src/cli.js';
-import game from '../src/index.js';
+import game, { random } from '../src/index.js';
 
 const name = askName();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 let answer = '';
 let result = 0;
 for (let i = 0; i < 3; i += 1) {
-  const task = Math.floor(Math.random(2, 3) * 100);
+  const task = random(2, 50);
   if (task % 2 === 0) answer = 'no';
   else answer = 'yes';
   result += game(task, answer, name);
