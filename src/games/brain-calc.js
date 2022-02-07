09461632520c87ee5fa2096ab1194302624cf86c
Dmitry Utkin <senigius8@gmail.com>
2022-02-07 17:17:31 +0300

    upd brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,29 +1,19 @@
 import askName from '../cli.js';
-import game, { random, congrats } from '../index.js';
+import game, {
+  random, congrats,
+  operandsForCalc, operationForCalc,
+} from '../index.js';
 
 const brainCalc = () => {
   const name = askName();
   let result = 0;
   console.log('What is the result of the expression?');
   for (let i = 0; i < 3; i += 1) {
-    let answer = 0;
     const operator1 = random(1, 10); // рандомим два оператора
     const operator2 = random(1, 10);
     let operation = random(0, 2); // рандомим цифру для операнда от 0 до 2
-    switch (operation) { // выбор операнда в зависимости от цифры
-      case 0:
-        operation = '+';
-        answer = operator1 + operator2;
-        break;
-      case 1:
-        operation = '-';
-        answer = operator1 - operator2;
-        break;
-      default:
-        operation = '*';
-        answer = operator1 * operator2;
-        break;
-    }
+    const answer = operandsForCalc(operation, operator1, operator2);
+    operation = operationForCalc(operation);
     const task = `${operator1} ${operation} ${operator2}`; // для вывода задачи пользователю
     result += game(task, answer, name);
     if (result > 4) break;
